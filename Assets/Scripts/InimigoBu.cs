2da7cc17cc004ba344a9ed615dbeddbc9f64a41c
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-29 00:20:44 -0300

    Implementando a parada do inimigo

--- Assets/Scripts/InimigoBu.cs
+++ Assets/Scripts/InimigoBu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class InimigoBu : MonoBehaviour
 {
+    private bool podeAndar;
+    private VerificarDiecaoDoMovimentoDoJogador oVerificarDiecaoDoMovimentoDoJogador;
+
 
     private GameObject oJogador;
     [SerializeField] private float velocidadeDoInimigo;
@@ -13,6 +16,7 @@ public class InimigoBu : MonoBehaviour
     private void Awake()
     {
         oJogador = GameObject.FindGameObjectWithTag("Player");
+        oVerificarDiecaoDoMovimentoDoJogador = oJogador.GetComponent<VerificarDiecaoDoMovimentoDoJogador>();
     }
 
     // Start is called before the first frame update
@@ -24,13 +28,42 @@ public class InimigoBu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        VerificarSePodeAndar();
         SeguirJogador();
         EspelharNaHorizontal();
     }
 
+    private void VerificarSePodeAndar()
+    {
+        if (indoParaDireita)
+        {
+            if (oVerificarDiecaoDoMovimentoDoJogador.indoParaDireita)
+            {
+                podeAndar = true;
+            }else
+            {
+                podeAndar = false;
+            }
+        }
+        else
+        {
+            if (oVerificarDiecaoDoMovimentoDoJogador.indoParaDireita)
+            {
+                podeAndar = false;
+            }
+            else
+            {
+                podeAndar = true;
+            }
+        }
+    }
+
     private void SeguirJogador()
     {
-        transform.position = Vector2.MoveTowards(transform.position, oJogador.transform.position, velocidadeDoInimigo * Time.deltaTime);
+        if (podeAndar)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, oJogador.transform.position, velocidadeDoInimigo * Time.deltaTime);
+        }
     }
 
     private void EspelharNaHorizontal()
